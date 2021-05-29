c2f78aa5da612f07385ca8827b00b11931ed8d88
Dani <danirg1414@outlook.com>
2021-05-29 13:25:54 -0500

    progressBar thread added

--- src/Thread/ProgressBarThread.java
+++ src/Thread/ProgressBarThread.java
@@ -1,9 +1,33 @@
 package Thread;
 
+import javafx.application.Platform;
 import model.ProgressBar;
+import ui.NaisuShoppingUI;
 
 public class ProgressBarThread extends Thread{
 	
 	private ProgressBar progressbar;
+	private NaisuShoppingUI naisu;
 	
+	public ProgressBarThread(ProgressBar progressB, NaisuShoppingUI naisuS) {
+		progressbar = progressB;
+		naisu = naisuS;
+	}
+	
+	public void run() {
+		while (progressbar.isActive()) {
+			progressbar.advance();
+			Platform.runLater(new Thread() {
+				public void run() {
+					naisu.updateBar();
+				}
+			});
+			try {
+				Thread.sleep(7);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+				
+	}
 }
--- src/ui/NaisuShoppingUI.java
+++ src/ui/NaisuShoppingUI.java
@@ -1,5 +1,28 @@
 package ui;
 
-public class NaisuShoppingUI {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+public class NaisuShoppingUI implements Initializable{
+	
+	//Loading Interface attributes
+	@FXML
+    private BorderPane mainBorderPane;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void updateBar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
