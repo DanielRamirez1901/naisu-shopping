7d3ecbfd0a7212caefea06656b6f70e78091eb5d
Dani <danirg1414@outlook.com>
2021-06-03 23:10:02 -0500

    2 BubbleSort methods added

--- src/model/Market.java
+++ src/model/Market.java
@@ -35,5 +35,33 @@ public class Market {
     	
     }
     
+    //Ordenamientos burbuja*************************************************
+    //Ordenamiento por nombre usuario
+    public void bubbleSortByUserName() {
+    	User userToOrder;
+    	for(int i = 0; i<(user.size()-1);i++) {
+    		for(int j = 0; j<(user.size()-1);j++) {
+    			if(user.get(j).getName().charAt(0)>user.get(j+1).getName().charAt(0)) {
+    				userToOrder = user.get(j);
+    				user.set(j, user.get(j+1));
+    				user.set(j+1, userToOrder);
+    			}
+    		}
+    	}
+    }
+    //Ordenamiento por apellido usuario
+    public void bubbleSortByUserLastname() {
+    	User userToOrder;
+    	for(int i = 0; i<(user.size()-1);i++) {
+    		for(int j = 0; j<(user.size()-1);j++) {
+    			if(user.get(j).getLastName().charAt(0)>user.get(j+1).getLastName().charAt(0)) {
+    				userToOrder = user.get(j);
+    				user.set(j, user.get(j+1));
+    				user.set(j+1, userToOrder);
+    			}
+    		}
+    	}
+    }
+  //**********************************************************************
     
 }
